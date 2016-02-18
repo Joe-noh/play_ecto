072893800a555549a2b42c729e40bbd9ca7a1dcd
Joe-noh <goflb.jh@gmail.com>
2016-02-19 00:11:43 +0900

    set repo mode manual before testing

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,3 +2,5 @@ ExUnit.start()
 
 Mix.Task.run "ecto.create", ~w(-r PlayEcto.Repo --quiet)
 Mix.Task.run "ecto.migrate", ~w(-r PlayEcto.Repo --quiet)
+
+Ecto.Adapters.SQL.Sandbox.mode(PlayEcto.Repo, :manual)
